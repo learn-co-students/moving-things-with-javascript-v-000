a655f936480da8e7aad824e9aabd30d3943a868d
Yechiel K <chilikkal@gmail.com>
2016-12-13 23:51:08 -0500

    made a moving dodger

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const app = "I don't do much."
+function moveDodgerRight() {
+  var leftNumber = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumber, 10)
+  if (left < 320) {
+      dodger.style.left = `${left + 1}px`
+  }
+}
+
+
+function moveDodgerLeft() {
+  var leftNumber = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumber, 10)
+  if (left > 0) {
+      dodger.style.left = `${left - 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+
+  switch (e.which) {
+    case 37:
+      moveDodgerLeft()
+      break;
+    case 39:
+      moveDodgerRight()
+      break;
+    default:
+
+  }
+})
